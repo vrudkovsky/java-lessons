a4a34ff07cec74af4c3bfdca755a1b6d8d9b866d
Victor Rudkovsky <vpcomtek@gmail.com>
2021-03-09 09:42:26 +0200

    -- lesson 08

--- src/main/java/lessonA4/LinkedListApp.java
+++ src/main/java/lessonA4/LinkedListApp.java
@@ -7,18 +7,18 @@ public class LinkedListApp {
         ll.addHead(12);
         ll.addHead(13);
         ll.addTail(20);
-//        ll.addTail(5);
-//        ll.addAfter(x -> x >= 10, 11);
-//        ll.addBefore(x -> x >= 10, 11);
-//        ll.remove(x -> x == 10);
+        ll.addTail(5);
+        ll.addAfter(x -> x >= 10, 11);
+        ll.addBefore(x -> x >= 10, 11);
+        ll.remove(x -> x == 10);
         ll.lenght_iter();
         System.out.println(ll.lenght_iter());
         System.out.println(ll.lenght_hr());
         System.out.println(ll.toString_itr());
 
-//        System.out.println(ll.contains(12));
-//        System.out.println(ll.contains(11));
-//        System.out.println(ll.contains2(12));
-//        System.out.println(ll.contains2(11));
+        System.out.println(ll.contains(12));
+        System.out.println(ll.contains(11));
+        System.out.println(ll.contains2(12));
+        System.out.println(ll.contains2(11));
     }
 }
